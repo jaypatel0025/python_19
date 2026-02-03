044ec5d73847aedb2bfbe9705d68a54575249096
jaypatel0025 <iamjaypatel259@gmail.com>
2026-02-03 12:53:56 +0530

    3-2-2026 Updated

---
+++ LambdaFunction.py
@@ -0,0 +1,13 @@
+ans=lambda y:y*y*y
+print("Cube of",5,"is:",ans(5))
+
+print("******************************")
+
+x=lambda a,b:a*b
+print(x(10,20))
+
+print("******************************")
+
+result=lambda num:"even" if num%2==0 else "odd"
+
+print(result(19))
